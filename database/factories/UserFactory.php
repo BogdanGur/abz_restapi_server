36f33eb682f917ddde17e97d9fda90f419e3ef34
Bogdan Gurskyi <35147686+BogdanGur@users.noreply.github.com>
2022-06-23 12:59:43 +0300

    Filling the Database with User data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,8 +20,10 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'phone' => '+380'.random_int(100000000, 999999999),
+            'position_id' => rand(1,4),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Str::random(60), // password
             'remember_token' => Str::random(10),
         ];
     }
